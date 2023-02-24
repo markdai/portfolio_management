44d37a0226a9f035d4d7ad604efa96b3482a4916
Mark Dai <daizhiwei89@hotmail.com>
2023-02-24 16:40:02 -0600

    Add option to use alternative API library

--- src/equity.py
+++ src/equity.py
@@ -51,6 +51,7 @@ import os
 from .logger import UseLogging
 from .eq_SQLite_utility import SQLiteRequest
 from .financial_API_utility import Stock, ETF
+# from .financial_API_utility_alternative import Stock, ETF
 
 
 class DbCommands(object):
